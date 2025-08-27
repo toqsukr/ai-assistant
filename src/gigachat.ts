566bb7df303b68f84d0fee5dad45fafe21995b3e
wastard <Ilya.pakylov@gmail.com>
2025-08-27 14:10:29 +0300

    added .env.example

--- src/gigachat.ts
+++ src/gigachat.ts
@@ -1,8 +1,6 @@
 import { GigaChat } from 'langchain-gigachat'
 
 export default new GigaChat({
-  credentials: process.env.AUTH_KEY,
-  scope: process.env.LLM_SCOPE,
   model: 'GigaChat-2',
   temperature: 0,
 })
